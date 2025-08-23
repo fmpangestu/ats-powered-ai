bd76cf15900785dd5f5f19236b4b058f4fbda545
Farhan Maulana Pangestu <farhanmaulana1710@gmail.com>
2025-08-24 04:37:04 +0700

    summary add

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -80,6 +80,7 @@ const Upload = () => {
     await kv.set(`resume:${uuid}`, JSON.stringify(data));
     setStatusText("Analysis complete, redirecting ...");
     console.log("DATA", data);
+    navigate(`/resume/${uuid}`);
   };
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
